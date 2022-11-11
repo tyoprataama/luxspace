f7a9fdde8759d069f1ca6cdae6bd1f4c8ec94a0f
tyoprataama <tyoprataama004@gmail.com>
2022-11-11 07:45:39 +0700

    setup tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./assets/css/app.css";
+import React from "react";
+
+function App() {
+  return ( <div className = "App" >
+    <h1> LUXME </h1> 
+    </div>
+  );
+}
+
+export default App;
