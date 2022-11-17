6712f3f53874d38a9095a559dbbfeb4ebc1d36c9
tyoprataama <tyoprataama004@gmail.com>
2022-11-17 11:31:43 +0700

    fixing bug on tailwind

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,8 @@
 module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   style: {
     postcss: {
-      plugins: [
-        require("tailwindcss")("./tailwind.config"),
-        require("autoprefixer"),
-      ],
+      plugins: [require('tailwindcss')('./tailwind.config'), require('autoprefixer')],
     },
   },
 };
